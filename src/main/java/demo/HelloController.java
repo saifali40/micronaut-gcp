5c051a110c6865e367e4a01e59f3c4a30153fca9
saif <saif.ali@anywhere.co>
2019-07-03 10:15:42 +0530

    first commit

---
+++ src/main/java/demo/HelloController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/")
+public class HelloController {
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String index() {
+        return "Hello World!";
+    }
+}
